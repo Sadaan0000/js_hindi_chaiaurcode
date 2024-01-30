290948fbbcc78f570d743c8d3e81ffd76449066c
Sadaan0000 <112737342+Sadaan0000@users.noreply.github.com>
2024-01-30 17:26:32 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14453
+let  accountEmail= "sadaanakhter123dhn@gmail.com"
+var accountPassword= " 12345"
+accountCity="dhanbad"
+let accountSate
+// accountId="12321"
+console.log( accountId);
+accountEmail="hacgvikfydgju"
+accountPassword="12212121121"
+accountCity="patna"
+console.table([accountId, accountEmail , accountPassword , accountCity, accountSate])
+/*
+    prefer not to use var because of issue in block and functional scope.
+*/
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//use all js code as newer version
+
+// alert("hello") we are using nodejs!!!!! code readibility should be high.first priority.
+
+// let name=Sadaan
+// let age=18
+// let isLogedIn=false
+
+// number >=2 to power 53
+// string=" "
+// boolean => true/false
+// null -- standalone value
+// undefined
+// symbol - unique 
+// object 
+console.log(typeof "sadaan");
